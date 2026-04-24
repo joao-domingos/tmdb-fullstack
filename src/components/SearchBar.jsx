8099e86edd421b30469d0c4b16b7dad678ee77f3
joao-domingos <joaopedro.mdomingos@gmail.com>
2026-04-24 10:50:55 -0300

    busca conseguindo fazer requisição pra api, nao teste ainda com outros parametros

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { TextField } from '@mui/material';
+import { useMovie } from '../context/MovieContext';
+
+function SearchBar() {
+  const { dispatch } = useMovie();
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      dispatch({ type: 'SET_FILTERS', payload: { query: e.target.value } });
+    }
+  };
+
+  return (
+    <TextField
+      label="Search Movies"
+      placeholder="Type a movie name and press Enter"
+      onKeyDown={handleSearch}
+      fullWidth
+      sx={{ mb: 2 }}
+    />
+  );
+}
+
+export default SearchBar;
