02fd3a64db1bd999d45ab638eab08036c0ac808f
joao-domingos <joaopedro.mdomingos@gmail.com>
2026-04-24 12:31:11 -0300

    ajustando ordem de layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { MovieProvider } from './context/MovieContext';
 import SearchBar from './components/SearchBar';
 import FilterSection from './components/FilterSection';
 import MovieGrid from './components/MovieGrid';
+import Pagination from './components/Pagination';
 
 function App() {
   return (
     <MovieProvider>
       <SearchBar />
       <FilterSection />
+      <Pagination />
       <MovieGrid />
     </MovieProvider>
   );
